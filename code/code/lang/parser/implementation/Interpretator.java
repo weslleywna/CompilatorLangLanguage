c371be2353a960df0eb1e61bc3d68bae709f1caf
Vitor Hugo N Pinto <vitorhugojf9@gmail.com>
2020-11-08 19:31:14 -0300

    mais um pouco pro santo

--- code/code/lang/parser/implementation/Interpretator.java
+++ code/code/lang/parser/implementation/Interpretator.java
@@ -26,32 +26,40 @@ public class Interpretator extends langBaseVisitor<Object> {
 
     @Override
     public Object visitData(langParser.DataContext ctx) {
+        //TODO: DATA TYPENAME KEYS_OPEN (decl)* KEYS_CLOSE
+
         return super.visitData(ctx);
     }
 
     @Override
     public Object visitDecl(langParser.DeclContext ctx) {
+        //TODO: IDENTIFIER DPDP type SEMI
+
         return super.visitDecl(ctx);
     }
 
     @Override
     public Object visitFunc(langParser.FuncContext ctx) {
+        //TODO: IDENTIFIER PARENTHESIS_OPEN (params)? PARENTHESIS_CLOSE (DP type (COMMA type)*)? KEYS_OPEN (cmd)* KEYS_CLOSE
+
         return super.visitFunc(ctx);
     }
 
     @Override
     public Object visitParams(langParser.ParamsContext ctx) {
+        //TODO: IDENTIFIER DPDP type (COMMA IDENTIFIER DPDP type)*
+
         return super.visitParams(ctx);
     }
 
     @Override
     public Object visitType(langParser.TypeContext ctx) {
-
+        //TODO: type BRACKET_OPEN BRACKET_CLOSE
         if (ctx.getChildCount() == 1) {
-            return super.visitBtype(ctx.btype());
+            return this.visitBtype(ctx.btype());
         }
 
-        return super.visitType(ctx);
+        throw new RuntimeException("unknown operator: " + ctx.getChild(0).toString());
     }
 
     @Override
@@ -82,13 +90,36 @@ public class Interpretator extends langBaseVisitor<Object> {
 
     @Override
     public Object visitCmd(langParser.CmdContext ctx) {
-
         if(ctx.getChild(0) == ctx.lvalue(0)) {
             Object valueLValue = visitLvalue(ctx.lvalue(0));
             Object valueExp = visitExp(ctx.exp(0));
 
             enviroment.peek().put(valueLValue.toString(), valueExp);
             System.out.println(enviroment.peek());
+        }
+
+        if(ctx.getChild(0) == ctx.KEYS_OPEN()){
+            //TODO: KEYS_OPEN (cmd)* KEYS_CLOSE
+        }
+        //TODO: IF PARENTHESIS_OPEN exp PARENTHESIS_CLOSE cmd
+        //TODO: IF PARENTHESIS_OPEN exp PARENTHESIS_CLOSE cmd ELSE cmd
+        if(ctx.getChild(0) == ctx.ITERATE()){
+            //TODO: ITERATE PARENTHESIS_OPEN exp PARENTHESIS_CLOSE cmd
+        }
+        if(ctx.getChild(0) == ctx.READ()){
+            //TODO: READ lvalue SEMI
+        }
+        if(ctx.getChild(0) == ctx.PRINT()){
+            //TODO: PRINT exp SEMI
+        }
+        if(ctx.getChild(0) == ctx.RETURN()){
+            //TODO: RETURN exp (COMMA exp)* SEMI
+        }
+        if(ctx.getChild(0) == ctx.lvalue()){
+            //TODO: lvalue EQ exp SEMI
+        }
+        if(ctx.getChild(0) == ctx.IDENTIFIER()){
+            //TODO: IDENTIFIER PARENTHESIS_OPEN (exps)? PARENTHESIS_CLOSE (LESSTHAN (DP)? lvalue (COMMA lvalue)* (DP)? MORETHAN)? SEMI;
         }
 
         return super.visitCmd(ctx);
@@ -170,7 +201,11 @@ public class Interpretator extends langBaseVisitor<Object> {
             case "!":
                 return !Boolean.valueOf(this.visitSexp(ctx.sexp()).toString());
             case "-":
-                return 0; //TODO: Q Q ESSE CARAI FAZ?
+                Object sexpResult = this.visitSexp(ctx.sexp());
+                if(sexpResult instanceof Integer)
+                    return Integer.parseInt(sexpResult.toString());
+                else
+                    return Float.parseFloat(sexpResult.toString());
         }
 
         if(ctx.getChild(0) == ctx.BOOLEAN()) {
@@ -203,13 +238,21 @@ public class Interpretator extends langBaseVisitor<Object> {
         if (ctx.getChildCount() == 1) {
             return this.visitLvalue(ctx.lvalue());
         }
-
         if (ctx.getChild(0) == ctx.PARENTHESIS_OPEN())
         {
             return this.visitExp(ctx.exp());
         }
+        if (ctx.getChild(0) == ctx.NEW())
+        {
+            //TODO: NEW type (BRACKET_OPEN (exp)? BRACKET_CLOSE)?
+        }
+        if (ctx.getChild(0) == ctx.IDENTIFIER())
+        {
 
-        return super.visitPexp(ctx);
+            //TODO: IDENTIFIER PARENTHESIS_OPEN (exps)? PARENTHESIS_CLOSE BRACKET_OPEN exp BRACKET_CLOSE
+        }
+
+        throw new RuntimeException("unknown operator: " + ctx.getChild(0).toString());
     }
 
     @Override
@@ -217,17 +260,22 @@ public class Interpretator extends langBaseVisitor<Object> {
         if(ctx.getChild(0) == ctx.IDENTIFIER()) {
             return ctx.getChild(0);
         }
+        if (ctx.getChildCount() == 4) {
+            //TODO: lvalue BRACKET_OPEN exp BRACKET_CLOSE
+        }
+        if (ctx.getChildCount() == 3) {
+            //TODO: lvalue DOT IDENTIFIER
+        }
 
-        return super.visitLvalue(ctx);
+        throw new RuntimeException("unknown operator: " + ctx.getChild(0).toString());
     }
 
     @Override
     public Object visitExps(langParser.ExpsContext ctx) {
         if(ctx.getChild(1) == ctx.COMMA()) {
-            return this.visitExp(ctx.exp(0));
+            return this.visitExp(ctx.exp(1));
         }
-
-        return super.visitExp(ctx.exp(0));
+        return this.visitExp(ctx.exp(0));
     }
 
     public Object CalculateArithmeticValue(Object exp1, Object exp2, String operator){
